24d9c66068bb7556a5519f9e097f20933ffc3f66
Erik Neuberger <erik.neuberger@smail.th-koeln.de>
2025-01-07 16:23:18 +0100

    remove test

--- forcefeedback/src/main.cpp
+++ forcefeedback/src/main.cpp
@@ -2,14 +2,10 @@
 #include "FeedbackFuncs.h"
 #include "MotorDriver.h"
 
-int timestep = 0;
-
-const int intervalMs = 20;
+const int intervalMs = 10;
 unsigned long lastMicros = 0;
 
-MotorDriver motorDriver(34, 14, 12, 1700, true);
-
-void test();
+MotorDriver motorDriver(34, 14, 12, 1400, true);
 
 void setup() {
   Serial.begin(115200);
@@ -22,23 +18,6 @@ void loop()
     motorDriver.update();
     lastMicros = currentMicros;
   }
-}
-
-
-
-void test() {
-  double sinValue = sin(timestep/10.0);
-  Serial.print(">sinValue:");
-  Serial.println(sinValue);
-
-  float mapValue = mapFloat(sinValue, 0, 1, 0, 255);
-  Serial.print(">mapValue:");
-  Serial.println(mapValue);
-
-  if (sinValue > 0) {
-    analogWrite(9,mapValue);
-  }
-  else {
-    analogWrite(10,abs(mapValue));
-  }
+  Serial.print(">timeTaken:");
+  Serial.println(micros() - currentMicros);
 }
