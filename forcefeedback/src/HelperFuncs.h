30379fdf1fea0e48a4cfe49ad35e4b48d9cdb2f0
Erik Neuberger <erik.neuberger@smail.th-koeln.de>
2025-01-16 23:20:27 +0100

    rename and add funcs

---
+++ forcefeedback/src/HelperFuncs.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <math.h>
+#include <Arduino.h>
+
+float mapFloat(float x, float in_min, float in_max, float out_min, float out_max) {
+    return out_min + ((x - in_min) * (out_max - out_min)) / (in_max - in_min);
+}
+
+double sawtooth(double t, double period, double amplitude) {
+    if (period <= 0) {
+        return 0.0; // Avoid division by zero
+    }
+
+    // Wrap time t within one period using fmod
+    double fractionalPart = fmod(t, period);
+    if (fractionalPart < 0) {
+        fractionalPart += period; // Handle negative times
+    }
+
+    // Map to range [-amplitude, +amplitude]
+    return amplitude * (2.0 * (fractionalPart / period) - 1.0);
+}
+
+double sineWave(double t, double period, double amplitude) {
+    if (period <= 0) {
+        return 0.0; // Avoid division by zero
+    }
+
+    // Convert period to angular frequency
+    double omega = 2.0 * PI / period;
+
+    // Evaluate sine wave
+    return amplitude * sin(omega * t);
+}
